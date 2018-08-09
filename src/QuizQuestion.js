23ea2c89087992dc82ef444f7cf4a424cb37d0c8
mdeckersscgp <41964874+mdeckersscgp@users.noreply.github.com>
2018-08-09 11:25:09 -0500

    incorrect answer

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,9 +3,17 @@ import QuizQuestionButton from './QuizQuestionButton.js';
 
 class QuizQuestion extends Component
 {
+    constructor(props){
+        super(props);
+        this.state = {incorrectAnswer: false};
+    }
+
     handleClick = (buttonText) => {
         if(buttonText === this.props.quiz_question.answer){
+            this.setState({incorrectAnswer: false})
              this.props.showNextQuestionHandler();
+        } else{
+            this.setState({incorrectAnswer: true})
         }
     }
 
@@ -22,6 +30,10 @@ class QuizQuestion extends Component
                             clickHandler={this.handleClick.bind(this)} />)}
                     </ul>
                 </section>
+                {
+                    this.state.incorrectAnswer ? 
+                    <p className="error">Sorry, that's not right</p> : null
+                }
             </main>
         )
     }
